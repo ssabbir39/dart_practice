3bd180ca03d7d97311227cab872ea3c99279c270
sabbir <sabbircvgc11@gmail.com>
2025-09-15 08:13:47 +0600

    arithmetic operators

---
+++ bin/arithmetic_operators.dart
@@ -0,0 +1,12 @@
+void main(){
+  var a = 10;
+  var b = 3;
+
+
+  print("a+b = ${a+b}");  // Add: +
+  print("a-b = ${a-b}");  // Subtract: -
+  print("a*b = ${a*b}");  // Multiply: *
+  print("a/b = ${a/b}");  // Divide: /
+  print("a%b = ${a%b}");  // vagshesh ber korar jonno use kori modulas sign %
+
+}
