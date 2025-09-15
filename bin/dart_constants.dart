45c81727b333d55458e04db6810c8f8442f139dd
sabbir <sabbircvgc11@gmail.com>
2025-09-15 09:34:24 +0600

    unary operators & dart const & final uses

---
+++ bin/dart_constants.dart
@@ -0,0 +1,25 @@
+void main(){
+
+  const thisYear = "This year is : 2025";
+  //final thisYear = "This year is : 2025";
+
+
+  print(thisYear);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//constants means drubok,mane value poriborton hoy na
+// const diyeo kora jay final diyeo kora jay
---
+++ bin/unary_operators.dart
@@ -0,0 +1,38 @@
+void main(){
+
+  var x = 10;
+
+  // when we need increment we use ++
+  // when we need decrement we use --
+
+
+  // when we use unarary oparator sign after variable like x++ then initially we see main value and then one by one increment
+
+  print(x++);
+  print(x++);
+  print(x++);
+  print(x++);
+
+  // when we use unarary oparator sign before variable like ++x then we dont see initial value, automatically increment one by one
+
+  // print(++x);
+  // print(++x);
+  // print(++x);
+  // print(++x);
+
+
+  // when we use unarary oparator sign after variable like x-- then initially we see main value and then one by one decrement
+
+  // print(x--);
+  // print(x--);
+  // print(x--);
+  // print(x--);
+
+  // when we use unarary oparator sign before variable like --x then we dont see initial value, automatically decrement one by one
+
+  // print(--x);
+  // print(--x);
+  // print(--x);
+  // print(--x);
+
+}
