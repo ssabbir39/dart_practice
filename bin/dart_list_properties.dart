935108389a33b3bcf22be2642923e6221b56cb08
sabbir <sabbircvgc11@gmail.com>
2025-09-16 16:30:42 +0600

    dart  list properties

---
+++ bin/dart_list_properties.dart
@@ -0,0 +1,22 @@
+void main(){
+
+  var city = ['dhaka ' , 'chittagong' , 'khulna' , 'cumilla'];
+
+  var result = city.length;
+
+  print(result);
+}
+
+
+/* list properties e variable er sathe dot diye
+
+first - it returns the first element case
+isEmpty - it returns true if the list is empty
+isNotEmpty - it returns true ifthe list has at lest one element
+length - it returns the length if the list
+last - it returns the last element of the list
+reversed - it returns a list in reverse order
+single - it check if the list has only one element and returns it
+
+lekhle oita kaj hbe
+ */
